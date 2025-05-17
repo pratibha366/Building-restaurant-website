e7616181766cd66e72cf6e7b9fc8d2421665b861
pratibha366 <pratibhaluitel122@gmail.com>
2025-05-17 21:25:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+# Building-restaurant-website
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $("header").addClass("sticky");
+        } else {
+            $("header").removeClass("sticky");
+        }
+    });
+
+    function newDate() {
+        return new Date().getFullYear();
+    }
+
+    window.onload = function () {
+        document.getElementById("autodate").innerHTML = newDate();
+    };
+});
